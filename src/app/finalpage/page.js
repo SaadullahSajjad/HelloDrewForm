ac6047320b4399545057509a7912eb0e1b40904c
Najeeb <najeeb.nedo5555@gmail.com>
2025-03-16 01:51:39 +0500

    thankyou msg changed

--- src/app/finalpage/page.js
+++ src/app/finalpage/page.js
@@ -5,14 +5,29 @@ const finalpage = () => {
         {/* Left Section (Centering content) */}
         <div className="bg-white p-8 sm:p-12 rounded-lg w-full sm:w-1/2 h-full flex flex-col items-center justify-center">
           <h1 className="text-3xl sm:text-4xl font-semibold text-center text-gray-800 mb-4 sm:mb-6">
-            You’re Ready to Roll! 🎉
+            You're In. (Almost.)
           </h1>
           <p className="text-lg text-center text-gray-600 mb-4 sm:mb-6">
-            Welcome to <span className="text-blue-500">Drew’s</span> world.
+            We've got your application, and our team is reviewing it now. If you make the cut, you'll get 30 days of exclusive, free access to test-drive Hello Drew before the world gets in on April 15.
           </p>
-          <p className="text-center text-gray-600">
-            Thanks for submitting your details! Your personalized quote is on
-            the way.
+          <p className="text-center text-gray-600 mb-4 sm:mb-6">
+            No gimmicks. No commitments. Just AI that actually works.
+          </p>
+          
+          <div className="mt-4 mb-4">
+            <h2 className="text-xl font-semibold text-center text-gray-800 mb-2">
+              What's Next?
+            </h2>
+            <p className="text-center text-gray-600 mb-2">
+              👉 If you're approved, expect an invite soon, keep an eye on your inbox.
+            </p>
+            <p className="text-center text-gray-600 mb-2">
+              👉 If this isn't your round, no worries. We'll still keep you in the loop and make sure you're among the first to get access when we open the doors again.
+            </p>
+          </div>
+          
+          <p className="text-center text-gray-600 mt-2">
+            Either way, you're now on our radar, and big things are coming. Stay tuned.
           </p>
         </div>
 
